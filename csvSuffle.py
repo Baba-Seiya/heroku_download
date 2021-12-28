8bea714e357179a79ca1c7aab8a9108f1675885b
seiya-baba <c0a2012791@edu.teu.ac.jp>
2021-12-29 04:35:06 +0900

    python追加

---
+++ csvSuffle.py
@@ -0,0 +1,11 @@
+from random import randint
+import time
+mise = ["店名","店名２","店名３"]
+while True:
+    msg = ""
+    with open("info.csv","w",encoding="utf_8",newline='\n') as fileobj:
+        fileobj.write("店名,混雑率,フラグ")
+        for i in mise:
+            msg += i + "," + randint(0,100) + "," + randint(0,1) + "\n"
+        fileobj.write(msg)
+    time.sleep(30)
